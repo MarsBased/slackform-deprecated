c81d0f0ae8fd0767bdf88387ca64c6eaa68b5af5
Oriol Collell <oriol.collell.bcn@gmail.com>
2015-11-17 11:11:06 +0100

    create directory structure following the structure of a gem

---
+++ lib/slackform.rb
@@ -0,0 +1,7 @@
+require 'yaml'
+require 'httparty'
+
+require 'slackform/api_gateway/slack'
+require 'slackform/api_gateway/typeform'
+require 'slackform/slack_inviter'
+require 'slackform/configuration'
